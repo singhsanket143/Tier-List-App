80011e2362d8804aaa6e29b63b42a89646a02391
Sanket Singh <singhsanket143@gmail.com>
2024-07-13 12:48:13 +0530

    Added tier list creation

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const tierInput = document.getElementById('tier');
 
 const submitBtn = document.getElementById('submit');
 
+const imageForm = document.getElementById('image-form');
+
+imageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+    const imageItemInput = document.getElementById('image-item');
+    if(tierInput.value === '') {
+        alert('Please enter a valid image url');
+        return;
+    }
+    const imageUrl = imageItemInput.value;
+    createTierListItem(imageUrl);
+    imageItemInput.value = '';
+});
+
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault(); // stops the default behaviour of the event
     // To get access of the element on which this event was fired
@@ -30,4 +45,18 @@ function createTierList(tierListName) {
 
     const tierSection = document.getElementById('tier-list-section');
     tierSection.appendChild(newTierList);
+}
+
+function createTierListItem(imageUrl) {
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('item-container');
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+
+    imageDiv.appendChild(img);
+
+    const nonTierSection = document.getElementById('non-tier-section');
+    nonTierSection.appendChild(imageDiv);
+
 }
