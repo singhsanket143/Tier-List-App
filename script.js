56d8e5814d7e528429199677b577b54948fc1a51
abhishekkumar085 <abhishek950820@gmail.com>
2024-07-15 13:59:13 +0530

    Feature:- Add edit functionality for tier list heading names #3

--- script.js
+++ script.js
@@ -10,105 +10,125 @@ const submitBtn = document.getElementById('submit');
 
 const imageForm = document.getElementById('image-form');
 
-
-for(const itemContainer of itemContainers) {
-    setUpItemContainerForDrag(itemContainer);
+for (const itemContainer of itemContainers) {
+  setUpItemContainerForDrag(itemContainer);
 }
 
 // tierLists.forEach(setUpDropZoneInTierList);
 
-
 imageForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const imageItemInput = document.getElementById('image-item');
-    if(imageItemInput.value === '') {
-        alert('Please enter a valid image url');
-        return;
-    }
-    const imageUrl = imageItemInput.value;
-    createTierListItem(imageUrl);
-    imageItemInput.value = '';
+  event.preventDefault();
+  const imageItemInput = document.getElementById('image-item');
+  if (imageItemInput.value === '') {
+    alert('Please enter a valid image url');
+    return;
+  }
+  const imageUrl = imageItemInput.value;
+  createTierListItem(imageUrl);
+  imageItemInput.value = '';
 });
 
 submitBtn.addEventListener('click', (event) => {
-    event.preventDefault(); // stops the default behaviour of the event
-    // To get access of the element on which this event was fired
-    if(tierInput.value === '') {
-        alert('Please enter a tier name');
-        return;
-    }
-    createTierList(tierInput.value);
-    tierInput.value = '';
+  event.preventDefault(); // stops the default behaviour of the event
+  // To get access of the element on which this event was fired
+  if (tierInput.value === '') {
+    alert('Please enter a tier name');
+    return;
+  }
+  createTierList(tierInput.value);
+  tierInput.value = '';
 });
 
 function createTierList(tierListName) {
-    const newTierList = document.createElement('div');
+  const newTierList = document.createElement('div');
 
-    newTierList.classList.add('tier-list');
+  newTierList.classList.add('tier-list');
 
-    const heading = document.createElement('div'); // Try to randomly assign color to this heading
-    heading.classList.add('heading');
+  const heading = document.createElement('div'); // Try to randomly assign color to this heading
+  heading.classList.add('heading');
 
-    const textContainer = document.createElement('div');
-    textContainer.textContent = tierListName;
+  const textContainer = document.createElement('div');
+  textContainer.textContent = tierListName;
+  //   textContainer.setAttribute('contentEditable', 'true');
 
-    heading.appendChild(textContainer);
+  //adding edit functionality to the tier-list-app
+  textContainer.addEventListener('dblclick', (event) => {
+    const text = event.target.innerHTML;
+    const inputItem = document.createElement('input');
+    inputItem.type = 'text';
+    inputItem.value = text;
+    inputItem.addEventListener('blur', () =>
+      saveEdit(textContainer, inputItem)
+    );
+    inputItem.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        saveEdit(textContainer, inputItem);
+      }
+    });
+    textContainer.innerHTML = '';
+    textContainer.appendChild(inputItem);
+    inputItem.focus();
+  });
 
-    const newTierListItems = document.createElement('div');
-    newTierListItems.classList.add('tier-list-items');
+  heading.appendChild(textContainer);
 
+  const newTierListItems = document.createElement('div');
+  newTierListItems.classList.add('tier-list-items');
 
-    newTierList.appendChild(heading);
-    newTierList.appendChild(newTierListItems);
+  newTierList.appendChild(heading);
+  newTierList.appendChild(newTierListItems);
 
-    setUpDropZoneInTierListItem(newTierListItems);
+  setUpDropZoneInTierListItem(newTierListItems);
 
-    const tierSection = document.getElementById('tier-list-section');
-    tierSection.appendChild(newTierList);
+  const tierSection = document.getElementById('tier-list-section');
+  tierSection.appendChild(newTierList);
+}
+
+// Edit tier list
+function saveEdit(textContainer, inputItem) {
+  textContainer.innerHTML = inputItem.value;
 }
 
 function createTierListItem(imageUrl) {
-    const imageDiv = document.createElement('div');
-    imageDiv.setAttribute('draggable', 'true');
-    imageDiv.classList.add('item-container');
+  const imageDiv = document.createElement('div');
+  imageDiv.setAttribute('draggable', 'true');
+  imageDiv.classList.add('item-container');
 
-    setUpItemContainerForDrag(imageDiv);
+  setUpItemContainerForDrag(imageDiv);
 
-    const img = document.createElement('img');
-    img.src = imageUrl;
+  const img = document.createElement('img');
+  img.src = imageUrl;
 
-    imageDiv.appendChild(img);
+  imageDiv.appendChild(img);
 
-    const nonTierSection = document.getElementById('non-tier-section');
-    nonTierSection.appendChild(imageDiv);
-
+  const nonTierSection = document.getElementById('non-tier-section');
+  nonTierSection.appendChild(imageDiv);
 }
 
 function setUpItemContainerForDrag(itemContainer) {
-    itemContainer.addEventListener('dragstart', (event) => {
-        console.log(event)
-        currentDraggedItem = event.target.parentNode;
-    });
+  itemContainer.addEventListener('dragstart', (event) => {
+    console.log(event);
+    currentDraggedItem = event.target.parentNode;
+  });
 
-    itemContainer.addEventListener('dblclick', (event) => {
-        const parentNode = event.target.parentNode;
-        const nonTierSection = document.getElementById('non-tier-section');
-        nonTierSection.appendChild(parentNode);
-    });
+  itemContainer.addEventListener('dblclick', (event) => {
+    const parentNode = event.target.parentNode;
+    const nonTierSection = document.getElementById('non-tier-section');
+    nonTierSection.appendChild(parentNode);
+  });
 }
 
 function setUpDropZoneInTierListItem(tierListItem) {
-    tierListItem.addEventListener('drop', (event) => {
-        event.preventDefault(); // stops the default behaviour of the event which is to not allow drop
-    });
+  tierListItem.addEventListener('drop', (event) => {
+    event.preventDefault(); // stops the default behaviour of the event which is to not allow drop
+  });
 
-    tierListItem.addEventListener('dragover', function (event) {
-        // console.log(event.target);
-        // event.target.appendChild(currentDraggedItem);
-        console.log("event coming up", event);
-        if(this !== currentDraggedItem.parentNode) {
-            this.appendChild(currentDraggedItem);
-        }
-
-    });
+  tierListItem.addEventListener('dragover', function (event) {
+    // console.log(event.target);
+    // event.target.appendChild(currentDraggedItem);
+    console.log('event coming up', event);
+    if (this !== currentDraggedItem.parentNode) {
+      this.appendChild(currentDraggedItem);
+    }
+  });
 }
