88a9a57ed4d89938bd604f500089d8eb222c6207
hemanbhullar <102414075+hemanbhullar@users.noreply.github.com>
2024-07-31 07:48:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for(const itemContainer of itemContainers) {
     setUpItemContainerForDrag(itemContainer);
 }
 
-// tierLists.forEach(setUpDropZoneInTierList);
+// tierLists.forEach(setUpDropZoneInTierListItem);  
 
 
 imageForm.addEventListener('submit', (event) => {
@@ -46,25 +46,34 @@ function createTierList(tierListName) {
 
     newTierList.classList.add('tier-list');
 
-    const heading = document.createElement('div'); // Try to randomly assign color to this heading
-    heading.classList.add('heading');
-
-    const textContainer = document.createElement('div');
-    textContainer.textContent = tierListName;
-
-    heading.appendChild(textContainer);
+    const heading = document.createElement('h1'); // Try to randomly assign color to this heading
+    heading.textContent = tierListName;
 
     const newTierListItems = document.createElement('div');
     newTierListItems.classList.add('tier-list-items');
 
+    const deleteImagediv = document.createElement('div');
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = "https://cdn-icons-png.freepik.com/512/3807/3807871.png";
+    deleteImagediv.classList.add("delete-icon");
+
+    deleteImagediv.appendChild(deleteIcon);
 
     newTierList.appendChild(heading);
     newTierList.appendChild(newTierListItems);
-
+    newTierList.appendChild(deleteImagediv);
     setUpDropZoneInTierListItem(newTierListItems);
 
     const tierSection = document.getElementById('tier-list-section');
     tierSection.appendChild(newTierList);
+    DeleteTierList(tierSection, deleteImagediv, newTierList);
+}
+
+function DeleteTierList(tierSection, deleteImagediv, newTierList){
+    deleteImagediv.addEventListener('click', () => {
+        tierSection.removeChild(newTierList);
+    })
+
 }
 
 function createTierListItem(imageUrl) {
@@ -72,7 +81,7 @@ function createTierListItem(imageUrl) {
     imageDiv.setAttribute('draggable', 'true');
     imageDiv.classList.add('item-container');
 
-    setUpItemContainerForDrag(imageDiv);
+    console.log(imageDiv);
 
     const img = document.createElement('img');
     img.src = imageUrl;
@@ -82,11 +91,12 @@ function createTierListItem(imageUrl) {
     const nonTierSection = document.getElementById('non-tier-section');
     nonTierSection.appendChild(imageDiv);
 
+    setUpItemContainerForDrag(imageDiv);
 }
 
 function setUpItemContainerForDrag(itemContainer) {
     itemContainer.addEventListener('dragstart', (event) => {
-        console.log(event)
+        console.log("Start dragging");
         currentDraggedItem = event.target.parentNode;
     });
 
@@ -98,17 +108,18 @@ function setUpItemContainerForDrag(itemContainer) {
 }
 
 function setUpDropZoneInTierListItem(tierListItem) {
-    tierListItem.addEventListener('drop', (event) => {
+    tierListItem.addEventListener('dragover', (event) => {
+        console.log("dragover");
         event.preventDefault(); // stops the default behaviour of the event which is to not allow drop
     });
 
-    tierListItem.addEventListener('dragover', function (event) {
+    tierListItem.addEventListener('drop', function (event) {
         // console.log(event.target);
         // event.target.appendChild(currentDraggedItem);
         console.log("event coming up", event);
+        console.log(currentDraggedItem);
         if(this !== currentDraggedItem.parentNode) {
             this.appendChild(currentDraggedItem);
         }
-
     });
 }
